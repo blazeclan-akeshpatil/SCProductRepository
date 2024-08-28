4aa317ccaacc81a102452124f22ffb4912364060
akesh.patil <akesh.patil@blazeclan.com>
2024-08-28 20:09:21 +0800

    Release v1.4

--- s3bucket/main.tf
+++ s3bucket/main.tf
@@ -1,18 +1,28 @@
-variable "bucket_name" {
+variable "operating_system" {
+  description = "Select OS for the instance.\nValid values are Linux,Windows"
+  validation {
+    condition = contains(["Linux", "Windows"], var.operating_system)
+    error_message = "Valid values for var: operating_system are (Linux, Windows)."
+  }
   type = string
 }
 
 variable "instance_type" {
   type        = string
-  description = "Choose Instance Type.Valid values are item1,item2 and item3"
+  description = "Choose allowed instance type.\nAllowed values are t2.small, t2.medium, t3.large"
   validation {
-    condition = contains(["item1", "item2", "item3"], var.instance_type)
-    error_message = "Valid values for var: test_variable are (item1, item2, item3)."
+    condition = contains(["t2.small", "t2.medium", "t3.large"], var.instance_type)
+    error_message = "Valid values for var: instance_type are (t2.small, t2.medium, t3.large)."
   }
+}
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+  bucket = "demo-bucket-${random_string.random.result}"
 }
 
 output regional_domain_name {
