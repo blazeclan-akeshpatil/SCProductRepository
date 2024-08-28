157ed96a97ab2be1611b3263a5961fc892f3eaf2
akesh.patil <akesh.patil@blazeclan.com>
2024-08-28 20:35:38 +0800

    Release v1.7

--- s3bucket/main.tf
+++ s3bucket/main.tf
@@ -1,5 +1,5 @@
 variable "operating_system" {
-  description = "Select OS for the instance.\nValid values are Linux,Windows"
+  description = "Select OS for the instance.Valid values are Linux, Windows"
   validation {
     condition = contains(["Linux", "Windows"], var.operating_system)
     error_message = "Valid values for var: operating_system are (Linux, Windows)."
@@ -9,7 +9,7 @@ variable "operating_system" {
 
 variable "instance_type" {
   type        = string
-  description = "Choose allowed instance type.\nAllowed values are t2.small, t2.medium, t3.large"
+  description = "Choose allowed instance type.Allowed values are t2.small, t2.medium, t3.large"
   validation {
     condition = contains(["t2.small", "t2.medium", "t3.large"], var.instance_type)
     error_message = "Valid values for var: instance_type are (t2.small, t2.medium, t3.large)."
@@ -18,7 +18,7 @@ variable "instance_type" {
 
 variable "instance_group" {
   type        = string
-  description = "Choose suitable group of instance.\nAllowed values are Group_1, Group_2, Group_3"
+  description = "Choose suitable group of instance.Allowed values are Group_1, Group_2, Group_3"
   validation {
     condition = contains(["Group_1", "Group_2", "Group_3"], var.instance_group)
     error_message = "Valid values for var: instance_group are (Group_1, Group_2, Group_3)."
@@ -26,9 +26,9 @@ variable "instance_group" {
 }
 
 resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
+  length      = 5
+  special     = false
+  min_special = 0
 }
 
 resource "aws_s3_bucket" "bucket" {
